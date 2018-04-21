40daa97c0cc333dc251362eab8e335aa0eebafca
wangleiR <1220632559@qq.com>
2018-04-21 11:31:08 +0800

    completed the practice1

--- src/main/java/practice1/Security.java
+++ src/main/java/practice1/Security.java
@@ -12,27 +12,9 @@ public class Security {
 
     public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
 
-        boolean isAccess = false;
-        if (user == null) {
-            return isAccess;
+        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)||securityChecker.isAdmin()) {
+           return true;
         }
-
-        if (permission == null) {
-            return isAccess;
-        }
-
-        if (permissions.size() == 0) {
-            return isAccess;
-        }
-
-        if (securityChecker.isAdmin()) {
-            isAccess = true;
-        }
-
-        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
-            isAccess = true;
-        }
-
-        return isAccess;
+        return false;
     }
 }
